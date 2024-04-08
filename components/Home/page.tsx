83f8658d5a852cae0a5fb1451e2ce32a17e4bc8e
Tidings-SP <suryaprakashspro@gmail.com>
2024-04-08 12:03:53 +0530

    Firebase

--- components/Home/page.tsx
+++ components/Home/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Card,
   CardContent,
@@ -7,42 +8,106 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { DividerHorizontalIcon } from "@radix-ui/react-icons";
+import { collection, and, where, onSnapshot, query } from "firebase/firestore";
 import { Check, ChevronsRightLeft } from "lucide-react";
+import { useEffect, useState } from "react";
+import { db } from "../Authentication/firebase";
 
-export default function homepage() {
+export default function HomePage() {
+  const [unV, setUnV] = useState<{
+    id: string,
+    date: string,
+    link: string,
+    status: boolean,
+  }[]>([]);
+
+  const [v, setV] = useState<{
+    id: string,
+    date: string,
+    link: string,
+    status: boolean,
+  }[]>([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "reel"),
+      and(
+        where("status", "==", true),
+      ));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setV(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          date: doc.data().date,
+          link: doc.data().link,
+          status: doc.data().status,
+
+        }))
+      );
+    });
+
+    return () => unsubscribe(); // Cleanup when the component unmounts
+  }, []);
+
+  useEffect(() => {
+    const q = query(collection(db, "reel"),
+      and(
+        where("status", "==", false),
+      ));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setUnV(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          date: doc.data().date,
+          link: doc.data().link,
+          status: doc.data().status,
+
+        }))
+      );
+    });
+
+    return () => unsubscribe(); // Cleanup when the component unmounts
+  }, []);
   return (
     <div>
       <div>
-        <h1 className=" text-xl font-bold p-5">Un Verified videos </h1>
-        <Card className=" flex flex-col sm:flex-row m-6 border-blue-300">
-          <CardFooter className="flex flex-col p-4 sm:w-[40%] ">
-            <img className="object-cover rounded-lg" src="https://www.internetmatters.org/wp-content/uploads/2020/08/https___specials-images.forbesimg.com_imageserve_5f2b4c9efc3ed7b0659d27ae_0x0.jpg" alt="" />
-          </CardFooter>
-          <CardContent className="flex flex-col m-6 justify-between sm:w-[45%] ">
-            <CardTitle className=" text-center font-bold text-xl">Scam reel published in instagram</CardTitle>
-            <CardDescription className="text-left text-lg">reel published on Aug 5 , 2016</CardDescription>
-            <p className=" pt-10 ">Link-<a href="">https://rell.insta.com</a></p>
-          </CardContent>
-          <CardFooter>
-            <p className="text-lg"> <ChevronsRightLeft /> Un Verified</p>
-          </CardFooter>
-        </Card>
+        <h1 className=" text-xl font-bold p-5">Un-Verified videos </h1>
+        {unV.map((item) => (
+          <Card key={item.id} className=" flex flex-col sm:flex-row m-6 border-blue-300">
+            <CardFooter className="flex flex-col p-4 sm:w-[40%] ">
+              <img className="object-cover rounded-lg" src="https://www.internetmatters.org/wp-content/uploads/2020/08/https___specials-images.forbesimg.com_imageserve_5f2b4c9efc3ed7b0659d27ae_0x0.jpg" alt="" />
+            </CardFooter>
+            <CardContent className="flex flex-col m-2 justify-between sm:w-[45%] ">
+              <CardTitle className=" text-center font-bold text-xl">Fake content reel published in instagram</CardTitle>
+              <CardDescription className="text-left text-lg">Reel published on {item.date}</CardDescription>
+              <p className=" pt-10 ">Link-<a href="">{item.link}</a></p>
+            </CardContent>
+            <CardFooter>
+              <p className="flex flex-col items-center text-lg"> <ChevronsRightLeft /> Un-verified</p>
+            </CardFooter>
+          </Card>
+        ))}
+
       </div>
       <div>
         <h1 className=" text-xl font-bold p-5">Verified videos </h1>
-        <Card className="flex flex-col sm:flex-row m-6 border-blue-300  ">
-          <CardFooter className="flex flex-col p-4 sm:w-[40%] ">
-            <img className="object-cover rounded-lg " src="https://www.internetmatters.org/wp-content/uploads/2020/08/https___specials-images.forbesimg.com_imageserve_5f2b4c9efc3ed7b0659d27ae_0x0.jpg" alt="" />
-          </CardFooter>
-          <CardContent className="flex flex-col m-6 justify-between sm:w-[45%] ">
-            <CardTitle className=" text-center font-bold text-xl">Scam reel published in instagram</CardTitle>
-            <CardDescription className="text-left text-lg">reel published on Aug 5 , 2016</CardDescription>
-            <p className=" pt-10 ">Link-<a href="">https://rell.insta.com</a></p>
-          </CardContent>
-          <CardFooter>
-            <p className="text-lg  "><Check />Verified</p>
-          </CardFooter>
-        </Card>
+        {v.map((item) => (
+          <Card key={item.id} className="flex flex-col sm:flex-row m-6 border-blue-300  ">
+            <CardFooter className="flex flex-col p-4 sm:w-[40%] ">
+              <img className="object-cover rounded-lg " src="https://www.internetmatters.org/wp-content/uploads/2020/08/https___specials-images.forbesimg.com_imageserve_5f2b4c9efc3ed7b0659d27ae_0x0.jpg" alt="" />
+            </CardFooter>
+            <CardContent className="flex flex-col m-6 justify-between sm:w-[45%] ">
+              <CardTitle className=" text-center font-bold text-xl">Fake content reel published in instagram</CardTitle>
+              <CardDescription className="text-left text-lg">Reel published on {item.date}</CardDescription>
+              <p className=" pt-10 ">Link-<a href="">{item.link}</a></p>
+            </CardContent>
+            <CardFooter className="">
+              <div className="flex flex-col items-center text-lg  "><Check />Verified</div>
+            </CardFooter>
+          </Card>
+        ))}
+
       </div>
     </div>
 
